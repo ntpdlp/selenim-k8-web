9c285a53de0049d4781b32b38c2eb1bbfbd48e67
ntpdlp <30646772+ntpdlp@users.noreply.github.com>
2022-07-22 03:42:03 -0500

    lesson 17: find element from element

--- src/test/java/api_learning/L17ElementFindAnotherElement.java
+++ src/test/java/api_learning/L17ElementFindAnotherElement.java
@@ -4,19 +4,36 @@ import driver.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import url.Urls;
+
+import java.util.List;
 
 public class L17ElementFindAnotherElement implements Urls {
 
+    private static By figureSel = By.cssSelector(".figure");
+    private static By figureLinkSel = By.cssSelector(".figcaption a");
+    private static By figureLabelSel = By.cssSelector(".figcaption h5");
     public static void main(String[] args) {
 
         WebDriver driver = DriverFactory.getChromeDriver();
+        Actions actions = new Actions(driver);
+        WebElement figureLink;
+        WebElement figureLabel;
         try{
-            driver.get(baseUrl.concat(iframeSlug));
-            WebElement iframe = driver.findElement(By.cssSelector("[id$=\"_ifr\"]"));
-            switchToEmbeddedIframe(driver,iframe);
-            returnBackDefaultFrame(driver);
-            Thread.sleep(2000);
+            driver.get(baseUrl.concat(hoverSlug));
+            List<WebElement> figureElems = driver.findElements(figureSel);
+            System.out.println(figureElems.size());
+            for (WebElement figureElem : figureElems) {
+                figureLink = figureElem.findElement(figureLinkSel);
+                figureLabel = figureElem.findElement(figureLabelSel);
+
+                actions.moveToElement(figureElem).perform();
+                Thread.sleep(1000);
+                System.out.println(figureLabel.getText());
+            }
+
+            Thread.sleep(1000);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -24,17 +41,4 @@ public class L17ElementFindAnotherElement implements Urls {
         driver.quit();
     }
 
-    public static void switchToEmbeddedIframe(WebDriver driver, WebElement iframe){
-        driver.switchTo().frame(iframe);
-        WebElement paragraphElem = driver.findElement(By.id("tinymce"));
-        paragraphElem.click();
-        paragraphElem.clear();
-        paragraphElem.sendKeys("hello, summer !!");
-    }
-
-    public static void returnBackDefaultFrame(WebDriver driver){
-        driver.switchTo().defaultContent();
-        driver.findElement(By.linkText("Elemental Selenium")).click();
-        System.out.println(driver.getCurrentUrl());
-    }
 }
